403bbc0f3c78c19f614d890ffc9a122bd2215f2d
kellypowers <kpowers.nj@gmail.com>
2020-04-01 13:42:55 -0400

    adds user controller and sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    helper_method :current_user, :logged_in?
 
 
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
 
-    def user_signed_in?
+    def logged_in?
         !!current_user
     end
 end
