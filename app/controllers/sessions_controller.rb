ca61084b2f6810d61df1f61f640c301896d6016d
kellypowers <kpowers.nj@gmail.com>
2020-04-12 14:38:10 -0400

    fixes the login, end was in the wrong spot.  adds some styling to view pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,28 +8,29 @@ class SessionsController < ApplicationController
 
   def create # receives data submitted in login form, authenticates and logs in a valid user
     if auth
+      #binding.pry
       @user = User.find_or_create_by(uid: auth['uid']) do |u|
         u.name = auth['info']['name']
         u.email = auth['info']['email']
         u.password = SecureRandom.hex
-      end 
+      end
       session[:user_id] = @user.id
-      redirect_to '/users/home', flash[:message] = "You successfully logged in!" 
-      #@user = User.find_or_create_by_omniauth(auth_hash)
-      #session[:user_id] = @user.id
-      #redirect_to user_path(@user), flash[:message] = "You successfully logged in!" 
-    elsif @user = User.find_by(email: params[:email])
-      if @user && @user.authenticate(params[:password])
+      redirect_to '/users/home' 
+      
+      elsif @user = User.find_by(email: params[:email])
+         @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id # log in the user
         flash[:message] = "You successfully logged in! Welcome, #{@user.name}!"
         redirect_to user_path(@user) 
-      end
-    else # present login form so user can try logging in again
+      
+        else # present login form so user can try logging in again
       #need to define user so the message will show and so the errors have a variable to refer to
       @user = User.new
         flash[:message] = "Your login attempt was unsuccessful. Please enter a valid email and password combination or try signing up."
         render :new
+      
     end
+    
   end
 
 
